cdfd3cc8e0b379fb9afd26df74bba622c42f13df
fairwell-coding <coding@temmel.me>
2021-11-01 23:39:24 +0100

    NumOpt Assignment 1 - example 1, 3-1, 3-2, 4

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ IMPORTANT:
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from scipy.optimize import approx_fprime
+from scipy.optimize import approx_fprime, linprog
 from typing import Callable
 
 # Modify the following global variables to be used in your functions
@@ -20,7 +20,7 @@ from typing import Callable
 
 alpha = 1
 beta = 5
-epsilon = 10 ** -4  # used for 3.1 and 3.2
+epsilon = 10 ** -4  # used for tasks 3.1, 3.2 and 4
 
 d = 2.5
 n_dim = 5
@@ -277,15 +277,15 @@ def task3():
     """ Start of your code
     """
 
-    plot_solutions_task1(ax)
-    plot_solutions_task2(ax)
+    __plot_solutions_task1(ax)
+    __plot_solutions_task2(ax)
 
     """ End of your code
     """
     return fig
 
 
-def plot_solutions_task2(ax):
+def __plot_solutions_task2(ax):
     x = np.random.randn(5)
 
     width = 0.3
@@ -314,7 +314,7 @@ def plot_solutions_task2(ax):
     ax[6].bar(numerical_bars, approx_fprime(x, func_2c, epsilon), width)
 
 
-def plot_solutions_task1(ax):
+def __plot_solutions_task1(ax):
     x = np.random.randn(2)
 
     width = 0.3
@@ -345,10 +345,126 @@ def task4():
 
     """ Start of your code
     """
-    M = None
+
+    cpu = np.asarray([0.11, 0.13, 0.09, 0.12, 0.15, 0.14, 0.11, 0.12])
+    gpu = np.asarray([0.10, 0.13, 0.08, 0.13, 0.14, 0.14, 0.09, 0.13])
+    energy = np.concatenate([cpu, gpu])
+
+    A_ub = np.asarray([[0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # -x3 <= 0
+                       [0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # -x4 <= 0
+                       [0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # -x5 <= 0
+                       [0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # -x6 <= 0
+                       [0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0],  # -x7 <= 0
+                       [0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0],  # -y0 <= 0
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0],  # -y1 <= 0
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0],  # -y2 <= 0
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0],  # -y3 <= 0
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0],  # -y4 <= 0
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0],  # -y5 <= 0
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0],  # -y6 <= 0
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1],  # -y7 <= 0
+                       [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # -x0 <= -480
+                       [0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # -x1 <= -600
+                       [0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # -x2 <= -560
+                       [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],  # x0 + .. + x7 <= 4500
+                       [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],  # y0 + .. + y7 <= 4500
+                       ])
+
+    b_ub = np.asarray([0,  # -x3 <= 0
+                       0,  # -x4 <= 0
+                       0,  # -x5 <= 0
+                       0,  # -x6 <= 0
+                       0,  # -x7 <= 0
+                       0,  # -y0 <= 0
+                       0,  # -y1 <= 0
+                       0,  # -y2 <= 0
+                       0,  # -y3 <= 0
+                       0,  # -y4 <= 0
+                       0,  # -y5 <= 0
+                       0,  # -y6 <= 0
+                       0,  # -y7 <= 0
+                       -480,  # -x0 <= -480
+                       -600,  # -x1 <= -600
+                       -560,  # -x2 <= -560
+                       4500,  # x0 + .. + x7 <= 4500
+                       4500  # y0 + .. + y7 <= 4500
+                       ])
+
+    A_eq = np.asarray([[1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],  # x0 + y0 = 1200
+                       [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],  # x1 + y1 = 1500
+                       [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],  # x2 + y2 = 1400
+                       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],  # x3 + y3 = 400
+                       [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],  # x4 + y4 = 1000
+                       [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],  # x5 + y5 = 800
+                       [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0],  # x6 + y6 = 760
+                       [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],  # x7 + y7 = 1300
+                       ])
+
+    b_eq = np.asarray([1200,  # x0 + y0 = 1200
+                       1500,  # x1 + y1 = 1500
+                       1400,  # x2 + y2 = 1400
+                       400,  # x3 + y3 = 400
+                       1000,  # x4 + y4 = 1000
+                       800,  # x5 + y5 = 800
+                       760,  # x6 + y6 = 760
+                       1300  # x7 + y7 = 1300
+                       ])
+
+    optimization_result = linprog(c=energy, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, method='highs')
+    M = np.asmatrix([optimization_result['x'][:8], optimization_result['x'][8:]]).T
+
+    __verify_optimization_result(M)
+    __compute_total_energy_consumption(energy, optimization_result)
+    __find_optimal_result_for_task_g_constraints(A_eq, A_ub, b_eq, b_ub, energy)
+
     """ End of your code
     """
     return M
+
+
+def __find_optimal_result_for_task_g_constraints(A_eq, A_ub, b_eq, b_ub, energy):
+    b_ub[13] = -1200
+    b_ub[14] = -1500
+    b_ub[15] = -1400
+    b_ub[4] = -1300
+
+    task_g_optimization_result = linprog(c=energy, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, method='highs')
+
+    print('The result is infeasible (result = "{}").'.format(task_g_optimization_result['success']))  # Explanation: Executing processes P0, P1, P2 and P7 entirely on the CPU results in a total
+    # number of instructions 1200 + 1500 + 1400 + 1300 = 5400 which is bigger than one of your original constraints, i.e. x0 + ... + x7 <= 4500 instructions. Hence, no optimal solution can be
+    # computed without modifying the constraints.
+
+
+def __compute_total_energy_consumption(energy, optimization_result):
+    print('The total energy consumption for the computed scheduling plan is {} uWh.'.format(sum(energy * optimization_result['x'])))
+
+
+def __verify_optimization_result(M: np.matrix):
+    assert M[0, 0] + M[0, 1] == np.round(1200, decimals=5)  # x0 + y0 = 1200
+    assert M[1, 0] + M[1, 1] == np.round(1500, decimals=5)  # x1 + y1 = 1500
+    assert M[2, 0] + M[2, 1] == np.round(1400, decimals=5)  # x1 + y1 = 1400
+    assert M[3, 0] + M[3, 1] == np.round(400, decimals=5)  # x1 + y1 = 400
+    assert M[4, 0] + M[4, 1] == np.round(1000, decimals=5)  # x1 + y1 = 1000
+    assert M[5, 0] + M[5, 1] == np.round(800, decimals=5)  # x1 + y1 = 800
+    assert M[6, 0] + M[6, 1] == np.round(760, decimals=5)  # x1 + y1 = 760
+    assert M[7, 0] + M[7, 1] == np.round(1300, decimals=5)  # x1 + y1 = 1300
+    assert M[0, 0] >= np.round(480, decimals=5)  # x0 >= 480
+    assert M[1, 0] >= np.round(600, decimals=5)  # x1 >= 600
+    assert M[2, 0] >= np.round(560, decimals=5)  # x2 >= 560
+    assert M[3, 0] >= np.round(0, decimals=5)  # x3 >= 0
+    assert M[4, 0] >= np.round(0, decimals=5)  # x4 >= 0
+    assert M[5, 0] >= np.round(0, decimals=5)  # x5 >= 0
+    assert M[6, 0] >= np.round(0, decimals=5)  # x6 >= 0
+    assert M[7, 0] >= np.round(0, decimals=5)  # x7 >= 0
+    assert M[0, 1] >= np.round(0, decimals=5)  # y1 >= 0
+    assert M[2, 1] >= np.round(0, decimals=5)  # y2 >= 0
+    assert M[3, 1] >= np.round(0, decimals=5)  # y3 >= 0
+    assert M[4, 1] >= np.round(0, decimals=5)  # y4 >= 0
+    assert M[5, 1] >= np.round(0, decimals=5)  # y5 >= 0
+    assert M[6, 1] >= np.round(0, decimals=5)  # y6 >= 0
+    assert M[7, 1] >= np.round(0, decimals=5)  # y7 >= 0
+    assert M[0, 0] + M[1, 0] + M[2, 0] + M[3, 0] + M[4, 0] + M[5, 0] + M[6, 0] + M[7, 0] <= np.round(4500, decimals=5)  # x0 + .. + x7 <= 4500
+    assert M[0, 1] + M[1, 1] + M[2, 1] + M[3, 1] + M[4, 1] + M[5, 1] + M[6, 1] + M[7, 1] <= np.round(4500, decimals=5)  # y0 + .. + y7 <= 4500
 
 
 if __name__ == '__main__':
